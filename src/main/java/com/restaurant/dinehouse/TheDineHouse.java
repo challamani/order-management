6b1f56855cac382f3834445ca87cde6bfe8e7c01
Mani Challa <mani.challa1990@gmail.com>
2023-01-02 23:54:21 +0000

    todo-list

--- src/main/java/com/restaurant/dinehouse/TheDineHouse.java
+++ src/main/java/com/restaurant/dinehouse/TheDineHouse.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TheDineHouse {
 
+    /***
+     * TODO
+     * 1. Transaction table for order payments & expenses
+     * 2. Inventory table
+     * 3. Order payment receipt printing */
     //start-up event to load system-users
     public static void main(String[] args) {
         SpringApplication.run(TheDineHouse.class, args);
